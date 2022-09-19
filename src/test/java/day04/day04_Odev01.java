b15b591c662ff82a657e0920f43c48e81061ba57
selcukAltinisik <selcukaltin3544@gmail.com>
2022-09-19 18:21:13 +0300

    19Eyl22-18:20

--- src/test/java/day04/day04_Odev01.java
+++ src/test/java/day04/day04_Odev01.java
@@ -1,6 +1,6 @@
 package day04;
 
-public class C01_Odev01 {
+public class day04_Odev01 {
 
 
     public static void main(String[] args) {
---
+++ src/test/java/practice/FirstClass.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+
+
+    }
+}
